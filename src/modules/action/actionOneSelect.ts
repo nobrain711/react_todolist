9eca1c8ad0cf6e9d99e2d3a9bebac5f66df6d8ef
nobrain711 <qazx9975@ync.ac.kr>
2024-05-13 15:36:19 +0900

    code file add comment

--- src/modules/action/actionOneSelect.ts
+++ src/modules/action/actionOneSelect.ts
@@ -1,5 +1,11 @@
 import { ONE_SELECT } from "./actionType";
 
+/**
+ * todolist one select
+ *
+ * @param id number
+ * @returns {type}
+ */
 export const oneSelect = (id: number) => ({
   type: ONE_SELECT,
   payload: id,
